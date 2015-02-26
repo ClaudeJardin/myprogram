69621b809a5385a162847f3b76a72692a1c2749e
ClaudeJulien <spiculate@hotmail.com>
2015-02-26 16:09:41 +0800

    Upload Level 1
    Upload Level 1

---
+++ Project1/Project1/Source.cpp
@@ -0,0 +1,56 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define MAX 100
+int main(void)
+{
+	int n;
+	int i, j, k, count;
+	bool state=true;
+	int answer=0, pre;
+	int ans[MAX];
+	int data[MAX];
+	int check[MAX];
+	scanf("%d", &n);
+
+	for( i=0; i<n; i++)
+	{
+		scanf("%d", &data[i]);
+	}
+	for( i=0; i<n; i++)
+	{
+
+
+		for(k=data[i]-1; k>0; k--)
+		{
+			for(j=1; j<=data[i]; j++)
+			{
+				check[j]=j;
+			}
+			state=true;
+			count=data[i];
+			pre=0;
+			while(state)
+			{
+				answer=(count+pre)%k+1;
+				answer=check[answer];
+				if(answer==data[i]&&count!=1)
+					state=false;
+				else if(count==1)
+				{
+					ans[i]=k;
+				}
+				for(j=answer; j<count; j++)
+				{
+					check[j]=check[j+1];
+				}
+				pre=check[answer];
+				count--;
+			}
+		}
+
+	}
+	for(i=0; i<n; i++)
+		printf("%d\n", ans[i]);
+	system("pause");
+	return 0;
+}
